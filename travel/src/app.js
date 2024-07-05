e7c6cb960cc09be60c3eb6ff275ab0f702943b1c
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-07-06 00:39:32 +0530

    Imported Home.jsx file

--- travel/src/app.js
+++ travel/src/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './app.css'
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home"
 
 const App = () => {
     return (
         <>
         <Navbar/>
+        <Home/>
         
         </>
     )
