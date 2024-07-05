b82940f78716fc47ef8f5748674a2b4a365fcdab
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-07-06 00:44:08 +0530

    "Updated Home component with video and styling, and modified Navbar CSS and map files."

--- travel/src/Components/Home/Home.jsx
+++ travel/src/Components/Home/Home.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import './home.css'
+import video from '../../Assets/Video1.mp4'
 
 const Home = () => {
     return (
-        <div>home</div>
+        <section className="home">
+            <div className="overlay">
+                <video src={video} muted autoPlay loop type = "Video/mp4"></video>
+            </div>
+
+            <div className="homeContent container">
+                <div className="textDiv">
+
+                    <span className="smallText">
+                        Our Packages
+                    </span>
+
+                    <h1 className="homeTitle">
+                        Search your Holiday
+                    </h1>
+                    
+                </div>
+            </div>
+        </section>
     )
 } 
 
