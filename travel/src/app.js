0c6476ba6eab9f24038aa1d9604791732a4a920b
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-07-07 13:10:58 +0530

    "Updated Home and Main components with new styles, icons, and layout changes"

--- travel/src/app.js
+++ travel/src/app.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './app.css'
 import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home"
+import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main"
 
 const App = () => {
     return (
         <>
         <Navbar/>
         <Home/>
-        
+        <Main/>
         </>
     )
 }
