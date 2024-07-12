637117570c3e75b29f2c39de82ca27f70aee4162
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-07-12 22:41:19 +0530

    Add scrolling effect

--- travel/src/Components/Home/Home.jsx
+++ travel/src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from 'react'
 import './home.css'
 import video from '../../Assets/video3.mp4'
 import { GrLocation } from "react-icons/gr";
@@ -9,11 +9,19 @@ import { SiTripadvisor } from "react-icons/si";
 import { BsListTask } from "react-icons/bs";
 import { TbApps } from "react-icons/tb";
 
-
-
+// Importing AOS (Animate On Scroll) library for scroll animations
+import Aos from 'aos'
+// Importing the AOS CSS file to apply default styles for animations
+import 'aos/dist/aos.css'
 
 
 const Home = () => {
+
+    // React hook to add a scroll animation
+    useEffect (()=> {
+        Aos.init({duration: 2000})
+    }, [])
+
     return (
         <section className="home">
             <div className="overlay">
@@ -23,21 +31,21 @@ const Home = () => {
             <div className="homeContent container">
                 <div className="textDiv">
 
-                    <span className="smallText">
+                    <span data-aos = "fade-up" className="smallText">
                         Our Packages
                     </span>
 
-                    <h1 className="homeTitle">
+                    <h1 data-aos = "fade-up" className="homeTitle">
                         Search your Holiday
                     </h1>  
                 </div>
 
-                <div className="cardDiv grid">
+                <div data-aos = "fade-up" className="cardDiv grid">
                     <div className="destinationInput">
                         <label htmlFor="city">Search your destination:</label>
                         <div className="input flex">
                             <input type="text" placeholder="Enter name hear..."/>
-                            <GrLocation className="icons" />
+                            <GrLocation className="icons"/>
                         </div>
                     </div>
                     
@@ -64,7 +72,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="homeFooterIcons flex">
+                <div data-aos = "fade-up" className="homeFooterIcons flex">
                     <div className="rightIcons">
                         <FiFacebook className="icons"/>
                         <AiOutlineInstagram className="icons"/>
